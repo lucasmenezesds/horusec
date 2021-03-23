fde92548be4e25829064c881f544d90b450b0634
Nathan Tavares Nascimento <nathan.nascimento@zup.com.br>
2021-03-23 10:27:58 -0300

    Testing LDAP login

--- e2e/cypress/src/integration/auth-ldap/without_application_admin_spec.ts
+++ e2e/cypress/src/integration/auth-ldap/without_application_admin_spec.ts
@@ -1,5 +1,15 @@
-describe("My First Test", () => {
-    it("Does not do much!", () => {
-        expect(true).to.equal(true);
+describe("Horusec LDAP test", () => {
+    it("Should login with ldap credentials", () => {
+        cy.visit("http://localhost:8043/auth");
+        cy.wait(4000);
+
+        // Login with default account
+        cy.get("#email").type("test");
+        cy.get("#password").type("test");
+        cy.get("button").first().click();
+        cy.wait(1000);
+
+        // Check if not exists workspace
+        cy.contains("Add a new Workspace to start using Horusec.").should("exist");
     });
 });
